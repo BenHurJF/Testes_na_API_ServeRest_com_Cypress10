969a97933a1b658cc1057fb60492333235978fe7
BenHurJF <beiujeffer@hotmail.com>
2022-08-29 05:47:25 -0300

    alterações

--- cypress/e2e/api_products/GET_Products.cy.js
+++ cypress/e2e/api_products/GET_Products.cy.js
@@ -9,4 +9,18 @@ describe('test to find products', () => {
             expect(response.body.produtos.length).to.be.above(1);
         })
     });
+
+    it('find a one product specific', () => {
+        cy.constructBody().then((body) => {
+            cy.POSTProduct(URLS_PRODUCT.postProduct, body)
+        }).then((id) => {
+            const ID_PROD = id.body._id;
+            let nomeProd = JSON.parse(id.requestBody);
+           nomeProd = nomeProd.nome;
+           cy.GETProducts(URLS_PRODUCT.getOneProduct + ID_PROD).then((response) => {
+            let prodConsulta = response.body.nome;
+              expect(nomeProd).to.be.equal(prodConsulta);
+           })
+        })
+    });
 });
--- cypress/shared/urls-api-serverest/produtos.url.js
+++ cypress/shared/urls-api-serverest/produtos.url.js
@@ -1,5 +1,6 @@
 module.exports = Object.freeze({
     getProduct: '/Produtos',
+    getOneProduct: '/Produtos/',
     postProduct: '/Produtos',
     putProduct: '/Produtos/',
     deleteProduct: '/Produtos/'
